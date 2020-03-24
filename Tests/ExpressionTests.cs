dbe97e27f54c4659a2808722964bbc26c6dcbd84
Zev Spitz <shivisi@hotmail.com>
2020-03-24 16:47:11 +0200

    Add unit tests

---
+++ Tests/ExpressionTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using static System.Linq.Expressions.Expression;
+using ZSpitz.Util;
+using System.Linq.Expressions;
+
+namespace Tests {
+    public class ExpressionTests {
+        [Fact]
+        public void ExtractValueFromLambda() {
+            Expression<Func<bool>> expr = () => true;
+            Assert.Equal(true, expr.ExtractValue());
+        }
+
+        [Fact]
+        public void ExtractVariableFromLambda() {
+            bool b = new Random().NextDouble() >= .5;
+            Expression<Func<bool>> expr = () => b;
+            Assert.Equal(b, expr.ExtractValue());
+        }
+
+        [Fact]
+        public void ExtractValueFromExpression() {
+            var expr = Field(null, typeof(string).GetField("Empty"));
+            Assert.Equal("", expr.ExtractValue());
+        }
+    }
+}
