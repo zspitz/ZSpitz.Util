cb97e56c04eb2562fbb07ee459259abb4b7b2053
Zev Spitz <shivisi@hotmail.com>
2020-03-30 09:16:35 +0300

    Add NotifyChanged extension method

---
+++ ZSpitz.Util.Wpf.Core/Extensions/INotifyPropertyChanged.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZSpitz.Util.Wpf.Core.Extensions {
+    public static class INotifyPropertyChangedExtensions {
+        // https://stackoverflow.com/a/60668668/111794
+        public static void NotifyChanged<T>(
+                this INotifyPropertyChanged inpc,
+                ref T current, T newValue,
+                PropertyChangedEventHandler handler,
+                [CallerMemberName] string? name = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(current, newValue)) { return; }
+            current = newValue;
+            handler?.Invoke(inpc, new PropertyChangedEventArgs(name));
+        }
+    }
+}
