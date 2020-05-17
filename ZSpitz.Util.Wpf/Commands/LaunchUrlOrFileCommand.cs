e484e5083e372f4e8232b0f196757153396db90f
Zev Spitz <shivisi@hotmail.com>
2020-05-18 00:33:36 +0300

    Add LaunchUriOrFileCommand

---
+++ ZSpitz.Util.Wpf/Commands/LaunchUrlOrFileCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+using ZSpitz.Util;
+
+namespace ZSpitz.Util.Wpf {
+    public class LaunchUrlOrFileCommand : ICommand {
+        public bool CanExecute(object parameter) =>
+            parameter switch {
+                Uri _ => true,
+                string _ => true,
+                _ => false
+            };
+
+        public void Execute(object parameter) {
+            var psi = parameter switch {
+                Uri uri => new ProcessStartInfo(uri.ToString()),
+                string urlPath => new ProcessStartInfo(urlPath),
+                (string url, string args) => new ProcessStartInfo(url, args),
+                _ => null
+            };
+            if (psi is null || psi.FileName.IsNullOrWhitespace()) { return; }
+            psi.UseShellExecute = true;
+            Process.Start(psi);
+        }
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
