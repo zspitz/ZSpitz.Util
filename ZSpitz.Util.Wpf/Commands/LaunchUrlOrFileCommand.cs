ec58c41040ac1a740d6b0114e60cfbb6ebd678c9
Zev Spitz <shivisi@hotmail.com>
2020-05-18 01:16:45 +0300

    Allow binding to hyperlink

--- ZSpitz.Util.Wpf/Commands/LaunchUrlOrFileCommand.cs
+++ ZSpitz.Util.Wpf/Commands/LaunchUrlOrFileCommand.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Windows.Documents;
 using System.Windows.Input;
 using ZSpitz.Util;
 
 namespace ZSpitz.Util.Wpf {
     public class LaunchUrlOrFileCommand : ICommand {
-        public bool CanExecute(object parameter) =>
-            parameter switch {
-                Uri _ => true,
-                string _ => true,
-                _ => false
-            };
+        public bool CanExecute(object parameter) => !(
+                parameter switch {
+                    Hyperlink link => link.NavigateUri.ToString(),
+                    Uri uri => uri.ToString(),
+                    string s => s,
+                    (string url, string _) => url,
+                    _ => ""
+                }
+            ).IsNullOrWhitespace();
 
         public void Execute(object parameter) {
-            var psi = parameter switch {
-                Uri uri => new ProcessStartInfo(uri.ToString()),
-                string urlPath => new ProcessStartInfo(urlPath),
-                (string url, string args) => new ProcessStartInfo(url, args),
-                _ => null
-            };
+            ProcessStartInfo psi;
+            if (parameter is (string url, string args)) {
+                psi = new ProcessStartInfo(url, args);
+            } else {
+                psi = new ProcessStartInfo(parameter switch {
+                    Uri uri => uri.ToString(),
+                    string urlPath => urlPath,
+                    Hyperlink link => link.NavigateUri.ToString(),
+                    _ => null
+                });
+            }
             if (psi is null || psi.FileName.IsNullOrWhitespace()) { return; }
             psi.UseShellExecute = true;
             Process.Start(psi);
