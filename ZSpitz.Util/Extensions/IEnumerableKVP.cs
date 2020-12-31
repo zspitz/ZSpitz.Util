929e704997085c915fa569429f25e4d4ff944bcb
Zev Spitz <shivisi@hotmail.com>
2020-12-31 17:50:51 +0200

    SelectKVP fix

--- ZSpitz.Util/Extensions/IEnumerableKVP.cs
+++ ZSpitz.Util/Extensions/IEnumerableKVP.cs
@@ -6,7 +6,7 @@ namespace ZSpitz.Util {
     public static class IEnumerableKVPExtensions {
         public static void AddRangeTo<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> toAdd, Dictionary<TKey, TValue> dict) where TKey : notnull => toAdd.ForEach(kvp => dict.Add(kvp.Key, kvp.Value));
         public static IEnumerable<TValue> Values<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> src) => src.Select(x => x.Value);
-        public static IEnumerable<TResult> SelectKVP<TKey, TValue, TResult>(this IDictionary<TKey, TValue> dict, Func<TKey, TValue, TResult> selector) => dict.Select(kvp => selector(kvp.Key, kvp.Value));
+        public static IEnumerable<TResult> SelectKVP<TKey, TValue, TResult>(this IEnumerable<KeyValuePair<TKey, TValue>> src, Func<TKey, TValue, TResult> selector) => src.Select(kvp => selector(kvp.Key, kvp.Value));
         public static IEnumerable<TKey> Keys<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> src) => src.Select(x => x.Key);
         public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> src) => src.ToLookup(kvp => kvp.Key, kvp => kvp.Value);
         public static IEnumerable<KeyValuePair<TKey, TValue>> WhereKVP<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> src, Func<TKey, TValue, bool> predicate) => src.Where(kvp => predicate(kvp.Key, kvp.Value));
