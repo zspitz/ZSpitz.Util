20304609b8de1caf8b36a3f988d4f948e2ebfa21
Zev Spitz <shivisi@hotmail.com>
2021-05-30 02:48:16 +0300

    Obsolete: Replace ForEachT with foreach + tuple deconstruction

--- ZSpitz.Util/Extensions/IEnumerableTuple.cs
+++ ZSpitz.Util/Extensions/IEnumerableTuple.cs
@@ -5,10 +5,10 @@ using System.Linq;
 namespace ZSpitz.Util {
     public static class IEnumerableTupleExtensions {
         public static void AddRangeTo<T1, T2>(this IEnumerable<(T1, T2)> src, IDictionary<T1, T2> dict) => dict.AddRange(src);
-        public static IEnumerable<(T1, T2)> ForEachT<T1,T2>(this IEnumerable<(T1, T2)> src, Action<T1,T2> action) => src.ForEach(x => action(x.Item1, x.Item2));
-        public static IEnumerable<(T1, T2)> ForEachT<T1, T2>(this IEnumerable<(T1, T2)> src, Action<T1, T2, int> action) => src.ForEach((x, index) => action(x.Item1, x.Item2, index));
-        public static IEnumerable<(T1, T2, T3)> ForEachT<T1, T2, T3>(this IEnumerable<(T1, T2, T3)> src, Action<T1, T2, T3> action) => src.ForEach(x => action(x.Item1, x.Item2, x.Item3));
-        public static IEnumerable<(T1, T2, T3, T4)> ForEachT<T1, T2, T3, T4>(this IEnumerable<(T1, T2, T3, T4)> src, Action<T1, T2, T3, T4> action) => src.ForEach(x => action(x.Item1, x.Item2, x.Item3, x.Item4));
+        [Obsolete("Use foreach and tuple deconstruction")] public static IEnumerable<(T1, T2)> ForEachT<T1,T2>(this IEnumerable<(T1, T2)> src, Action<T1,T2> action) => src.ForEach(x => action(x.Item1, x.Item2));
+        [Obsolete("Use foreach and tuple deconstruction")] public static IEnumerable<(T1, T2)> ForEachT<T1, T2>(this IEnumerable<(T1, T2)> src, Action<T1, T2, int> action) => src.ForEach((x, index) => action(x.Item1, x.Item2, index));
+        [Obsolete("Use foreach and tuple deconstruction")] public static IEnumerable<(T1, T2, T3)> ForEachT<T1, T2, T3>(this IEnumerable<(T1, T2, T3)> src, Action<T1, T2, T3> action) => src.ForEach(x => action(x.Item1, x.Item2, x.Item3));
+        [Obsolete("Use foreach and tuple deconstruction")] public static IEnumerable<(T1, T2, T3, T4)> ForEachT<T1, T2, T3, T4>(this IEnumerable<(T1, T2, T3, T4)> src, Action<T1, T2, T3, T4> action) => src.ForEach(x => action(x.Item1, x.Item2, x.Item3, x.Item4));
 
         public static string JoinedT<T1, T2>(this IEnumerable<(T1, T2)> src, string delimiter, Func<T1, T2, string> selector) =>
             src.Joined(delimiter, x => selector(x.Item1, x.Item2));
