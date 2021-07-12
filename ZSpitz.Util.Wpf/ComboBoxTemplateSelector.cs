ec47ab8d072807f3031977f0edbdec1dd4a8f1f3
Zev Spitz <shivisi@hotmail.com>
2021-07-12 08:43:27 +0300

    ComboBoxTemplateSelector

---
+++ ZSpitz.Util.Wpf/ComboBoxTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ZSpitz.Util.Wpf {
+    // https://stackoverflow.com/a/33421573/111794
+    public class ComboBoxTemplateSelector : DataTemplateSelector {
+        public DataTemplate? SelectedItemTemplate { get; set; }
+        public DataTemplateSelector? SelectedItemTemplateSelector { get; set; }
+        public DataTemplate? DropdownItemsTemplate { get; set; }
+        public DataTemplateSelector? DropdownItemsTemplateSelector { get; set; }
+
+        public override DataTemplate? SelectTemplate(object item, DependencyObject container) {
+            var itemToCheck = container;
+
+            // Search up the visual tree, stopping at either a ComboBox or
+            // a ComboBoxItem (or null). This will determine which template to use
+            while (itemToCheck is not (null or ComboBoxItem or ComboBox)) {
+                itemToCheck = VisualTreeHelper.GetParent(itemToCheck);
+            }
+
+            // If you stopped at a ComboBoxItem, you're in the dropdown
+            var inDropDown = (itemToCheck is ComboBoxItem);
+
+            return inDropDown ? 
+                DropdownItemsTemplate ?? DropdownItemsTemplateSelector?.SelectTemplate(item, container) :
+                SelectedItemTemplate ?? SelectedItemTemplateSelector?.SelectTemplate(item, container);
+        }
+    }
+}
