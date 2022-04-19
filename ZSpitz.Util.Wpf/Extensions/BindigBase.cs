d3fd47e9edc9ddf404a7a50a8f14c9a0c650ee47
Zev Spitz <shivisi@hotmail.com>
2022-04-20 02:33:58 +0300

    BindingBase.Path

---
+++ ZSpitz.Util.Wpf/Extensions/BindigBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Data;
+
+namespace ZSpitz.Util.Wpf {
+    public static class BindigBaseExtensions {
+        public static string? Path(this BindingBase bindingBase) =>
+            bindingBase switch {
+                Binding binding => binding.Path.Path,
+                MultiBinding multiBinding => multiBinding.Bindings.FirstOrDefault()?.Path(),
+                PriorityBinding priorityBinding => priorityBinding.Bindings.FirstOrDefault()?.Path(),
+                _ => throw new InvalidOperationException()
+            };
+    }
+}
